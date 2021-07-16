e1c7152254ffc641bb4af17fc48ba8fa03d7b45f
Alessandro018 <alessandrosilva325@gmail.com>
2021-07-15 22:56:59 -0300

    Product details

--- app/php/views/products/details.php
+++ app/php/views/products/details.php
@@ -11,29 +11,33 @@ include "../../../database/Connection.php";
 </head>
 <body>
     <?php include "../navbar.php";?>
-    <div class="d-flex flex-column m-auto pt-5" style="width: 90%">
-        <span>Telefones e celulares / Smartphones / Iphone</span>
-        <div class="d-flex justify-content-between m-auto mt-4" style="width: min(90vw,700px)">
-            <div class="">
-                <img src="../../app/assets/img/iphone-12-red.png" width="340" height="340" style="border: solid 1px">
+    <div class="custom-content">
+        <div class="d-flex flex-column px-5 pt-4" style="background-color: #f2f2f2;">
+            <div class="breadcrumb">
+               <a href="" class="active-link">Telefones e celulares</a>    <span class="link-separation">&#10095;</span>
+               <a href="" class="active-link">Smartphones</a>   <span class="link-separation">&#10095;</span>
+               <a class="desactive-link">Iphone</a>
             </div>
-            <div class="d-flex flex-column">
-                <div>
-                    <h3>Iphone 11 pro max 128GB</h3>
-                    <h4 class="price">R$ 7.829,90</h4>
-                </div>
-                <div class="mt-4 d-flex flex-column">
-                    <button>Comprar</button>
-                    <button class="mt-4">Adicionar ao carrinho</button>
-                </div>
-                <div class="mt-4 d-flex flex-column">
-                    <span>Calcular frente</span>
-                    <input type="text" name="" id="">
+            <div class="show-product-details">
+                <img src="../../app/assets/img/iphone-12-red.png">
+                <div class="product-info">
+                    <div>
+                        <h3 class="product-name">Iphone 11 pro max 128GB</h3>
+                        <h4 class="price">R$ 7.829,90</h4>
+                    </div>
+                    <div class="buttons">
+                        <button class="button buy-button">Comprar</button>
+                        <button class="button add-cart-button" class="mt-4">Adicionar ao carrinho</button>
+                    </div>
+                    <div class="shipping">
+                        <span>Calcular frente</span>
+                        <input type="text" name="" id="">
+                    </div>
                 </div>
             </div>
         </div>
-        <link type="text/css" rel="stylesheet" href="../../app/assets/css/style.css">
-        <link type="text/css" rel="stylesheet" href="../../../../admin/app/assets/css/bootstrap.min.css">
     </div>
+    <link type="text/css" rel="stylesheet" href="../../app/assets/css/style.css">
+    <link type="text/css" rel="stylesheet" href="../../../../admin/app/assets/css/bootstrap.min.css">
 </body>
 </html>
